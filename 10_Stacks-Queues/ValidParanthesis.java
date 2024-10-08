01e38ae1e139b5b613d94e858dbf1e5ebab9aff0
syedali237 <syedaliulhasan19@gmail.com>
2024-10-09 02:40:45 +0530

    added BT

--- 10_Stacks-Queues/ValidParanthesis.java
+++ 10_Stacks-Queues/ValidParanthesis.java
@@ -4,12 +4,52 @@ import java.util.Stack;
 public class ValidParanthesis {
 
     boolean isValid(String s) {
-
         Stack<Character> stack = new Stack<>();
 
-        for (char c = 0 ; c < s.length() ; c++){
-            
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            char top = stack.isEmpty() ? ' ' : stack.peek();
+
+            if (current == '(' || current == '{' || current == '[') {
+                stack.push(current);
+            } else if (current == ')' && top == '(' && !stack.isEmpty()) {
+                stack.pop();
+            } else if (current == ']' && top == '[' && !stack.isEmpty()) {
+                stack.pop();
+            } else if (current == '}' && top == '{' && !stack.isEmpty()) {
+                stack.pop();
+            } else {
+                return false;
+            }
         }
-        return 
+
+        return stack.isEmpty();
+    }
+
+    boolean isValid2(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()){
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(' ) {
+                        return false;
+                    }
+                }
+                if (ch == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                if (ch == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[' ) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
