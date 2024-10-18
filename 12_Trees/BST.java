7f56333bbef0fd262244326fffd9701e836a1f4d
syedali237 <syedaliulhasan19@gmail.com>
2024-10-19 02:25:18 +0530

    AVL

---
+++ 12_Trees/BST.java
@@ -0,0 +1,109 @@
+public class BST {
+
+    public class Node {
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node (int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BST() {
+
+    }
+
+
+  public int height() {
+    return height(root);
+  }
+
+    private int height(Node node){
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(int value){
+        root = insert(value, root);
+    }
+
+    private Node insert (int value, Node node){
+
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
+        return node;
+    }
+
+    public void populate(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populateSorted(int[] nums){
+        populateSorted(nums, 0, nums.length);
+    }
+
+    private void populateSorted(int[] nums, int start, int end){
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start+end)/2;
+
+        this.insert(nums[mid]);
+        populateSorted(nums, start, mid);
+        populateSorted(nums, mid + 1, end);
+    }
+
+    public boolean balanced(){
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node){
+        if (node == null) {
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display(){
+        display(root, "Root Node:  ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.getValue());
+
+        display(node.left, "Left Child of " + node.getValue() + " : ");
+        display(node.right, "Right Child of " + node.getValue() + " : ");
+    }
+}
--- 12_Trees/TreeMain.java
+++ 12_Trees/TreeMain.java
@@ -1,13 +1,24 @@
-
-import java.util.Scanner;
-
 public class TreeMain {
 
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(scanner);
-        tree.display();
-        tree.prettyDisplay();
+        // Scanner scanner =  new Scanner(System.in);
+        // BinaryTree tree = new BinaryTree();
+        // tree.populate(scanner);
+        // tree.display();
+        // tree.prettyDisplay();
+
+        // BST tree = new BST();
+        // // int[] nums = {5,2,7,1,4,6,9,8,3,10};
+        // int[] nums = {1,2,3,4,5,6,7,8,9};
+        // tree.populateSorted(nums);
+        // tree.display();
+
+        AVL tree = new AVL();
+        
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+
+        System.out.println(tree.height());
     }
 }
